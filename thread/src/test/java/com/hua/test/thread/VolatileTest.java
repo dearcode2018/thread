502493b75c8a474156e5f2994baf92d05dcb1dd0
qianye.zheng <qianye.zheng@wehotelglobal.com>
2020-10-28 13:39:41 +0800

    no message

---
+++ thread/src/test/java/com/hua/test/thread/VolatileTest.java
@@ -0,0 +1,332 @@
+/**
+ * 描述: 
+ * VolatileTest.java
+ * 
+ * @author qye.zheng
+ *  version 1.0
+ */
+package com.hua.test.thread;
+
+//静态导入
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.hua.test.BaseTest;
+
+
+/**
+ * 描述: 
+ * 
+ * @author qye.zheng
+ * VolatileTest
+ */
+//@DisplayName("测试类名称")
+//@Tag("测试类标签")
+//@Tags({@Tag("测试类标签1"), @Tag("测试类标签2")})
+public final class VolatileTest extends BaseTest {
+
+	
+	
+	
+	class NoVolatileJob implements Runnable {
+		// 没有volatile 修饰，运行后的线程一直持有私有值，没有去感知其他线程对它的修改
+		private boolean cancelled = false;
+		
+		/**
+		 * @description 
+		 * @author qianye.zheng
+		 */
+		@Override
+		public void run() {
+			System.out.println("job has been started.");
+			// 当前线程无法感知其他线程对cancelled的修改，会导致死循环
+			while (!cancelled) {}
+			System.out.println("job has been cancelled.");
+		}
+		
+		void cancel() {
+			this.cancelled = true;
+		}
+	}
+	
+	class VolatileJob implements Runnable {
+		// 添加了 volatile 确保了所有线程对某个线程对该字段修改后的可见性
+		private volatile boolean cancelled = false;
+		
+		/**
+		 * @description 
+		 * @author qianye.zheng
+		 */
+		@Override
+		public void run() {
+			System.out.println("job has been started.");
+			while (!cancelled) {}
+			System.out.println("job has been cancelled.");
+		}
+		
+		void cancel() {
+			this.cancelled = true;
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	//@DisplayName("test")
+	@Test
+	public void testRunNoVolatile() {
+		try {
+			/*
+			 * 可见性问题是因为代码没有给JIT编译器足够的提示而使得其认为状态变量cancelled只有一个线程对其进行访问
+			 */
+			NoVolatileJob job = new NoVolatileJob();
+			Thread t = new Thread(job);
+			t.start();
+			TimeUnit.SECONDS.sleep(5);
+			job.cancel();
+			t.join();
+			
+		} catch (Exception e) {
+			log.error("test =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	//@DisplayName("test")
+	@Test
+	public void testRunVolatile() {
+		try {
+			VolatileJob job = new VolatileJob();
+			Thread t = new Thread(job);
+			t.start();
+			TimeUnit.SECONDS.sleep(5);
+			job.cancel();
+			//t.join();
+			
+		} catch (Exception e) {
+			log.error("test =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	//@DisplayName("test")
+	@Test
+	public void testFinally() {
+		try {
+			String str = null;
+			str.length();
+		} finally {
+			System.out.println("finally");
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	//@DisplayName("test")
+	@Test
+	public void test() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("test =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testTemp")
+	@Test
+	public void testTemp() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testTemp=====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testCommon")
+	@Test
+	public void testCommon() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testCommon =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testSimple")
+	@Test
+	public void testSimple() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testSimple =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testBase")
+	@Test
+	public void testBase() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testBase =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: [每个测试-方法]开始之前运行
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("beforeMethod")
+	@Tag(" [每个测试-方法]结束之后运行")
+	@BeforeEach
+	public void beforeMethod() {
+		System.out.println("beforeMethod()");
+	}
+	
+	/**
+	 * 
+	 * 描述: [每个测试-方法]结束之后运行
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("afterMethod")
+	@Tag(" [每个测试-方法]结束之后运行")
+	@AfterEach
+	public void afterMethod() {
+		System.out.println("afterMethod()");
+	}
+	
+	/**
+	 * 
+	 * 描述: 测试忽略的方法
+	 * @author qye.zheng
+	 * 
+	 */
+	@Disabled
+	@DisplayName("ignoreMethod")
+	@Test
+	public void ignoreMethod() {
+		System.out.println("ignoreMethod()");
+	}
+	
+	/**
+	 * 
+	 * 描述: 解决ide静态导入消除问题 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("noUse")
+	@Disabled("解决ide静态导入消除问题 ")
+	private void noUse() {
+		String expected = null;
+		String actual = null;
+		Object[] expecteds = null;
+		Object[] actuals = null;
+		String message = null;
+		
+		assertEquals(expected, actual);
+		assertEquals(message, expected, actual);
+		assertNotEquals(expected, actual);
+		assertNotEquals(message, expected, actual);
+		
+		assertArrayEquals(expecteds, actuals);
+		assertArrayEquals(expecteds, actuals, message);
+		
+		assertFalse(true);
+		assertTrue(true);
+		assertFalse(true, message);
+		assertTrue(true, message);
+		
+		assertSame(expecteds, actuals);
+		assertNotSame(expecteds, actuals);
+		assertSame(expecteds, actuals, message);
+		assertNotSame(expecteds, actuals, message);
+		
+		assertNull(actuals);
+		assertNotNull(actuals);
+		assertNull(actuals, message);
+		assertNotNull(actuals, message);
+		
+		fail();
+		fail("Not yet implemented");
+		
+		dynamicTest(null, null);
+		
+		assumeFalse(false);
+		assumeTrue(true);
+		assumingThat(true, null);
+	}
+
+}
